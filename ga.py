a09cfec5938e4dcc7d5251950e725d59be5ba551
jamesperalta <jamesperalta35@gmail.com>
2020-04-03 04:30:17 -0600

    Updated Tilly

--- ga.py
+++ ga.py
@@ -4,24 +4,46 @@
 # March 2020
 import random
 import copy
-# import numpy as np
-# import EnvironmentHelpers.
 
 # Global Variables
-mutationRate = 0.1
-mutationRadius = 4
-nGenerations = 500
-rouletteFactor = 0.6
-structures_per_generation = 1000
-fountains_per_generation = 10
+mutation_rate = 0.10
+mutation_radius = 4
+roulette_factor = 0.80
+n_generations = 500
+structures_per_generation = 500
+
+MAX_ENERGY = 200
+GENOME_LENGTH = 243
+
+# State configurations
+UNBROKEN = 0
+BROKEN = 1
+OOB = 2  # out of bounds
+
+# Money to be earned
+ONE_DOLLAR = 1
+FIVE_DOLLARS = 5
+TEN_DOLLARS = 10
+
+# Edges in the map
+TOP_EDGE = 0
+BOTTOM_EDGE = 9
+LEFT_EDGE = 0
+RIGHT_EDGE = 9
+
+# Actions
+MOVE_UP = 0
+MOVE_DOWN = 1
+MOVE_LEFT = 2
+MOVE_RIGHT = 3
+MAKE_FIX = 4
+DO_NOTHING = 5
+MOVE_RANDOM = 6
 
 
-class Enviroment:
-    # 0 - not broken
-    # 1 - broken
-    # 2 - falls off edge
+class Environment:
     def __init__(self):
-        self.generate_new_world()
+        self.world = self.generate_new_world()
 
     def generate_new_world(self):
         new_world = []
@@ -29,104 +51,102 @@ class Enviroment:
             temp = []
             for j in range(10):
                 temp.append(random.randint(0, 1))
-            # print(temp)
             new_world.append(temp)
 
-        self.world = copy.deepcopy(new_world)
+        return new_world
 
-    def getIndex(self, x, y, world):
-        # print("x: ", x, " y: ", y)
-        north = 2 if x == 0 else world[x - 1][y]
-        east = 2 if y == 9 else world[x][y + 1]
-        south = 2 if x == 9 else world[x + 1][y]
-        west = 2 if y == 0 else world[x][y - 1]
+    def get_index(self, x, y, world):
+        north = OOB if x == TOP_EDGE else world[x - 1][y]
+        east = OOB if y == RIGHT_EDGE else world[x][y + 1]
+        south = OOB if x == BOTTOM_EDGE else world[x + 1][y]
+        west = OOB if y == LEFT_EDGE else world[x][y - 1]
         center = world[x][y]
 
         # [North, East, South, West, Below]
-
-        temp = north * (3**4)
-        temp += east * (3**3)
-        temp += south * (3**2)
-        temp += west * 3
-        temp += center
-        return temp
+        #  Generates a bit string containing the index of
+        #  the action in the genome depending on the current
+        #  state of this structures neighbors
+        state = north * (3 ** 4)
+        state += east * (3 ** 3)
+        state += south * (3 ** 2)
+        state += west * 3
+        state += center
+        return state  # Number between 0 and 242
 
     def evaluate(self, struct):
-        # iterate over 200 energy
+        row, col = 0, 0
 
-        x, y = 0, 0
+        average_earnings = []
+        # Evaluate on 10 different fountains
+        for i in range(0, 5):
+            earnings = 0
+            temp_world = copy.deepcopy(self.generate_new_world())
+            for i in range(MAX_ENERGY):
+                action = struct.get_move(self.get_index(row, col, temp_world))
 
-        struct.earnings = 0
-        for i in range(10):
-            self.generate_new_world()
-            tempWorld = copy.deepcopy(self.world)
-
-            for i in range(200):
-                # action = struct.phenotype(self.get_neighbors(x, y, tempWorld))
-                action = struct.getMove(self.getIndex(x, y, tempWorld))
-                # print(struct.genome)
-                # print("x: ", x, " y: ",  y, " action ", action)
-                # neigh = get_neighbors(x, y, tempWorld)
-                # action = getAction(neigh)
-
-                if action == 0:
-                    x -= 1
-                    if(x < 0):
-                        struct.earnings -= 5  # remove magic numbers
-                        x = 0
-                elif action == 1:
-                    x += 1
-                    if(x > 9):
-                        struct.earnings -= 5
-                        x = 9
-                elif action == 2:
-                    y -= 1
-                    if(y < 0):
-                        struct.earnings -= 5
-                        y = 0
-                elif action == 3:
-                    y += 1
-                    if(y > 9):
-                        struct.earnings -= 5
-                        y = 9
-                elif action == 4:
-                    if(tempWorld[x][y] == 0):
-                        struct.earnings -= 1
+                if action == MOVE_UP:
+                    row -= 1
+                    # If Tilly falls off of the top edge
+                    if row < TOP_EDGE:
+                        earnings -= FIVE_DOLLARS
+                        row = TOP_EDGE  # Place back on the top edge
+                elif action == MOVE_DOWN:
+                    row += 1
+                    # If Tilly falls off of the bottom edge
+                    if row > BOTTOM_EDGE:
+                        earnings -= FIVE_DOLLARS
+                        row = BOTTOM_EDGE  # Place back on the bottom edge
+                elif action == MOVE_LEFT:
+                    col -= 1
+                    # If Tilly falls off of the left edge
+                    if col < LEFT_EDGE:
+                        earnings -= FIVE_DOLLARS
+                        col = LEFT_EDGE  # Place back on the left edge
+                elif action == MOVE_RIGHT:
+                    col += 1
+                    # If Tilly falls off of the right edge
+                    if col > RIGHT_EDGE:
+                        earnings -= FIVE_DOLLARS
+                        col = RIGHT_EDGE  # Place back on the right edge
+                elif action == MAKE_FIX:
+                    # If tilly attempts to fix a tile that isn't broken
+                    if (temp_world[row][col] == UNBROKEN):
+                        earnings -= ONE_DOLLAR
                     else:
-                        tempWorld[x][y] = 0
-                        struct.earnings += 10
-                elif action == 5:
+                        temp_world[row][col] = UNBROKEN
+                        earnings += TEN_DOLLARS
+                elif action == DO_NOTHING:
                     pass
                 else:
                     choice = random.choice([True, False])
-                    if(choice):
+                    if choice:
                         choice = random.choice([True, False])
-                        if(choice):
-                            x -= 1
-                            if(x < 0):
-                                struct.earnings -= 5  # remove magic numbers
-                                x = 0
+                        if choice:
+                            row -= 1
+                            if row < TOP_EDGE:
+                                earnings -= FIVE_DOLLARS
+                                row = TOP_EDGE
                         else:
-                            x += 1
-                            if(x > 9):
-                                struct.earnings -= 5
-                                x = 9
+                            row += 1
+                            if row > BOTTOM_EDGE:
+                                earnings -= FIVE_DOLLARS
+                                row = BOTTOM_EDGE
                     else:
                         choice = random.choice([True, False])
-                        if(choice):
-                            y -= 1
-                            if(y < 0):
-                                struct.earnings -= 5  # remove magic numbers
-                                y = 0
+                        if choice:
+                            col -= 1
+                            if (col < LEFT_EDGE):
+                                earnings -= FIVE_DOLLARS
+                                col = LEFT_EDGE
                         else:
-                            y += 1
-                            if(y > 9):
-                                struct.earnings -= 5
-                                y = 9
-        struct.earnings /= 10
+                            col += 1
+                            if (col > RIGHT_EDGE):
+                                earnings -= FIVE_DOLLARS
+                                col = RIGHT_EDGE
 
-        # print("action: ", action, "", "Earnings", struct.earnings)
-        # print(moves)
+                average_earnings.append(earnings)
+
+        struct.earnings = int(sum(average_earnings) / len(average_earnings))
 
 
 class Structure:
@@ -134,79 +154,107 @@ class Structure:
         self.earnings = 0
         self.genome = actions[:]
 
-        if(len(self.genome) == 0):
+        if (self.genome == []):
             self.genome = list()
-            for i in range(243):
+            for i in range(GENOME_LENGTH):
                 self.genome.append(random.randint(0, 6))
 
-    def getMove(self, index):
+    def get_move(self, index):
         return self.genome[index]
 
     def crossover(self, mate):
-        childGenome = []
+        child_genome = []
 
-        crossoverPoint = random.randint(0, len(self.genome))
-        for i in range(0, crossoverPoint):
-            childGenome.append(self.genome[i])
+        # Might implement two point crossover here
+        crossover_point = random.randint(0, len(self.genome))
+        for i in range(0, crossover_point):
+            child_genome.append(self.genome[i])
 
-        for i in range(crossoverPoint, len(self.genome)):
-            childGenome.append(mate.genome[i])
+        for i in range(crossover_point, len(self.genome)):
+            child_genome.append(mate.genome[i])
 
-        return Structure(childGenome)
+        return Structure(child_genome)
 
     def mutate(self):
-        for i in range(len(self.genome)):
+        for i in range(0, GENOME_LENGTH):
             val = random.uniform(0, 1)
-            if(val < mutationRate):
-                radius = random.randint(-mutationRadius, mutationRadius)
-                self.genome[i] = (self.genome[i] + radius) % 7
+
+            if val < mutation_rate:
+                current_value = self.genome[i]
+
+                new_point = random.randint(
+                    current_value - mutation_radius, current_value + mutation_radius)
+
+                # if new_point < 0:
+                #     self.genome[i] = 0
+                # elif new_point > 6:
+                #     self.genome[i] = 6
+                # else:
+                #     self.genome[i] = new_point
+
+                # self.genome[i] = new_point % 7
 
 
 class GenePool:
-    def __init__(self, structures, numStructs, world):
+    def __init__(self, structures, num_structs, world):
         self.pool = structures[:]
-        self.poolSize = numStructs
-        self.environment = world  # Create new world ?
+        self.pool_size = num_structs
+        self.environment = world
+
+        # Initialize pool to contain random Genomes
         if self.pool == []:
-            for _ in range(self.poolSize):
+            for _ in range(self.pool_size):
                 self.pool.append(Structure([]))
 
+        # Generate probabilities for picking Genomes
         self.weights = [1]
+        for i in range(1, self.pool_size):
+            self.weights.append(self.weights[i - 1] * roulette_factor)
 
-        for i in range(1, self.poolSize):
-            self.weights.append(self.weights[i - 1] * rouletteFactor)
         self.best = self.test()
 
-    def selectStructures(self, num=2):
+    def select_structures(self, num=2):
         return random.choices(self.pool, self.weights, k=num)
 
     def test(self):
         for struct in self.pool:
             self.environment.evaluate(struct)
         self.pool.sort(key=lambda x: x.earnings, reverse=True)
-        # print(self.weights)
-        # print([struct.earnings for struct in self.pool])
 
         return self.pool[0]
 
-    def nextGen(self):
-        newPool = []
-        for _ in range(self.poolSize):
-            parents = self.selectStructures(300)
-            child = parents[0].crossover(parents[1])
+    def next_gen(self):
+        new_pool = []
+        for _ in range(self.pool_size):
+            parents = self.select_structures()
+            parent_zero = copy.deepcopy(parents[0])
+            parent_one = copy.deepcopy(parents[1])
+            child = parent_zero.crossover(parent_one)
             child.mutate()
 
-            newPool.append(child)
-        return GenePool(newPool, self.poolSize, self.environment)
+            new_pool.append(child)
+
+        print("NEXT GEN")
+        print(list(map(lambda x: x.earnings, new_pool)))
+        new_env = Environment()
+        return GenePool(new_pool, self.pool_size, new_env)
 
 
 def main():
-    env = Enviroment()
+    env = Environment()
     pool = GenePool([], structures_per_generation, env)
-    for gen in range(nGenerations):
+    for gen in range(n_generations):
         print("Generation: ", gen, "  best earnings score: ", pool.best.earnings)
-        print(pool.best.genome)
-        pool = pool.nextGen()
+        pool = pool.next_gen()
+
+    file_name = "generation_" + \
+                str(n_generations) + "_fitness_score_" + \
+                str(pool.best.earnings) + ".txt"
+
+    file = open(file_name, "w")
+    array = " ".join(map(str, pool.best.genome))
+    file.write(array)
+    file.close()
 
 
 main()
